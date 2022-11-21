56394ab88209b9f209fb46d32bad4fb03244d4b0
Analia Lujan Zunino <analia.zunino@accenture.com>
2022-11-21 15:16:06 -0300

    agregar a un elemeto nuevo atributos y luegoal doc

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,26 @@ const enlaces = document.querySelectorAll('.navegacion a');
 console.log(enlaces[0])
 enlaces[0].textContent = 'nuevo texto para enlaces';
 enlaces[0].classList.add('nueva-clase');
-enlaces[0].classList.remove('navegacion__enlace');
+//enlaces[0].classList.remove('navegacion__enlace');
 
 
 //getElemntById se usaba antes 
-
 const heading2 = document.getElementById('heading');
+
+//----------Generar un nuevo enlace---------------
+const nuevoEnlace = document.createElement('A') // USAR MAYUSCULAS
+
+//agregar el hredf
+nuevoEnlace.href = 'nuevo_enlace.htm'
+//agregar el texto
+nuevoEnlace.textContent = 'nuevo enlace'
+
+//agregar la clase
+nuevoEnlace.classList.add('navegacion__enlace')
+
+//agregar en el documento
+
+ const navegacion = document.querySelector('.navegacion');
+ navegacion.appendChild(nuevoEnlace);
+
+console.log(nuevoEnlace)
