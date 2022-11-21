cec354fd020efd5d2f79f3ec9a6c1063cf5468f8
Analia Lujan Zunino <analia.zunino@accenture.com>
2022-11-21 15:54:51 -0300

    eventos Input en formulario

--- js/scripts.js
+++ js/scripts.js
@@ -65,3 +65,25 @@ btnEnviar.addEventListener('click', function(e){
     console.log('enviando formulario')
 });
 
+//Eventos de los Inputs y TextArea
+
+const datos =  {
+    nombre: '',
+    email: '',
+    mensaje: ''
+}
+
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+
+
+nombre.addEventListener('input', leerTexto);
+email.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+function leerTexto(e) {
+    datos[e.target.id] = e.target.value;
+
+    console.log(datos);
+}
