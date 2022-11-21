aca1a8135527b4022f3fb51e897ee486c7f80a18
Analia Lujan Zunino <analia.zunino@accenture.com>
2022-11-21 14:34:59 -0300

    querySelector

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// querySelector
+//document habla de todo lo que esta en la etiqueta de html
+const heading = document.querySelector('.header__texto h2'); // 0 o 1 elemento
+console.log(heading);
+heading.textContent = 'Nuevo Heading'; //modifica el texto del selector
+//querySelectorALl
+
+
+//getElemntById
