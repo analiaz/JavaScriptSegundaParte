ac427787de2ce4f9587e1c1f1801360cf57ca1c8
Analia Lujan Zunino <analia.zunino@accenture.com>
2022-11-21 14:45:43 -0300

    QuerySelectorAll y viejo GetElemntById

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,15 @@
 const heading = document.querySelector('.header__texto h2'); // 0 o 1 elemento
 console.log(heading);
 heading.textContent = 'Nuevo Heading'; //modifica el texto del selector
+
 //querySelectorALl
+const enlaces = document.querySelectorAll('.navegacion a');
+console.log(enlaces[0])
+enlaces[0].textContent = 'nuevo texto para enlaces';
+enlaces[0].classList.add('nueva-clase');
+enlaces[0].classList.remove('navegacion__enlace');
 
 
-//getElemntById
+//getElemntById se usaba antes 
+
+const heading2 = document.getElementById('heading');
